d875cf03454a889fd68bd8549fac26700a05c357
OctupusPrime <rasiapypkin2014@gmail.com>
2025-09-09 17:08:36 +0200

    mobile-app init

---
+++ packages/eslint-config/expo.js
@@ -0,0 +1,13 @@
+import expoConfig from "eslint-config-expo/flat";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+import { config as baseConfig } from "./base.js";
+
+export const config = [
+  ...baseConfig,
+  expoConfig,
+  eslintPluginPrettierRecommended,
+  {
+    ignores: ["dist/*"],
+  },
+];

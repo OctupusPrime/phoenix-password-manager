cc3cfef123c544a6ab84cdc720536430aa6dfb5e
OctupusPrime <rasiapypkin2014@gmail.com>
2025-09-11 16:29:15 +0200

    added react-native-reanimated integration

--- apps/mobile-app/app/index.tsx
+++ apps/mobile-app/app/index.tsx
@@ -1,21 +1,51 @@
-import { useEffect } from 'react';
-import { Text, View, Button } from 'react-native';
+import Animated, {
+  useSharedValue,
+  withTiming,
+  useAnimatedStyle,
+  Easing,
+} from 'react-native-reanimated';
+import { Text, View, Button, StyleSheet } from 'react-native';
 
 import { useSQLiteContext } from '@/db/SQLite.context';
 
 export default function IndexPage() {
-  const { db, status, error, connect } = useSQLiteContext();
+  const { status, connect } = useSQLiteContext();
 
-  useEffect(() => {
-    console.log('db changed', db);
-  }, [db]);
+  const randomWidth = useSharedValue(10);
+
+  const config = {
+    duration: 500,
+    easing: Easing.bezier(0.5, 0.01, 0, 1),
+  };
+
+  const style = useAnimatedStyle(() => {
+    return {
+      width: withTiming(randomWidth.value, config),
+    };
+  });
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Welcome to Phoenix Password Manager</Text>
       <Text style={{ marginVertical: 16 }}>Db Status: {status}</Text>
-      <Text>DB error: {error}</Text>
       <Button title="Connect" onPress={connect} />
+
+      <Animated.View style={[styles.box, style]} />
+      <Button
+        title="toggle"
+        onPress={() => {
+          randomWidth.value = Math.random() * 350;
+        }}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 80,
+    backgroundColor: 'black',
+    margin: 30,
+  },
+});
